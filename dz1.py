89320a88cd7ca0735d0cbd9d0abb9634d73c12de
Sergey9911 <kormilin.sergey@gmail.com>
2024-10-05 22:09:17 +0300

    Update dz1.py

--- dz1.py
+++ dz1.py
@@ -3,7 +3,7 @@ print(9.99>9.98 and 1000!=1000.1) #2nd program
 print(2*2+2)  #3rd program
 print(2*(2+2))
 print(2*2+2==2*(2+2))
-print(int(float('123.456')*10)%10)
+print(int(float('123.456')*10)%10) #4th programm
 
 
 
